eba43edc451703ff2c65dcd9dd51c9f4b3a4b372
Sagar koyani <sagarkoyani4u@gmail.com>
2020-01-22 15:35:46 +0000

    x
    X

--- Swift_TableView_Contacts/Swift_TableView_Contacts/ExapandableNames.swift
+++ Swift_TableView_Contacts/Swift_TableView_Contacts/ExapandableNames.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Apple. All rights reserved.
 // http://5e285066120f820014bf43bd.mockapi.io/Api/configs
 https://apps.apple.com/us/app/ranking-nfl/id1495202978?ls=1
-https://drive.google.com/folderview?id=16zXYGDl73KCYorLPWZdahwxE4AMU_8g2
+https://drive.google.com/folderview?id=16zXYGDl73KCYorLPWZdahwxE4AMU_8g2se
 import Foundation
 struct ExpandableNames {
     
