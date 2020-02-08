cd0af91c47bdce12b3212dfb3402679c7c3cb35d
Sagar koyani <sagarkoyani4u@gmail.com>
2020-02-08 09:22:03 +0530

    Fd
    Dd

--- Swift_TableView_Contacts/Swift_TableView_Contacts/ExapandableNames.swift
+++ Swift_TableView_Contacts/Swift_TableView_Contacts/ExapandableNames.swift
@@ -9,7 +9,7 @@ https://apps.apple.com/us/app/ranking-nfl/id1495202978?ls=1
 https://drive.google.com/folderview?id=16zXYGDl73KCYorLPWZdahwxE4AMU_8g2se
 import Foundation
 struct ExpandableNames {
-    
+    https://www.dropbox.com/s/igxsqcte70jxy3g/Journal%20X%20Simple%20-%20Take%20Home%20Project.zip?dl=0
     var isExpanded: Bool
     let names:[String]
     
